63c8989dd6ab0fe15cf52a8198e519426fc370b7
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-02-04 14:51:27 +0530

    email-microservice-frontend-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LoginForm from './components/forms/LoginForm';
+import SignUpForm from './components/forms/SignUpForm';
+import EmailForm from './components/forms/EmailForm';
+import NotFoundForm from './components/forms/NotFoundForm';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/email" element={<EmailForm />} />
+        <Route path="*" element={<NotFoundForm />} />
+      </Routes>
+      <ToastContainer />
+    </Router>
   );
 }
 
